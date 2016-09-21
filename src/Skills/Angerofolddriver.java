2ffe6b5164904bfcdbcf86ca24bd801a60893603
HELLOooWORLD <535140575@qq.com>
2016-09-21 22:58:07 +0800

    111
    111

---
+++ src/Skills/Angerofolddriver.java
@@ -0,0 +1,10 @@
+package Skills;
+
+/**
+ * Created by HKLLC on 2016/9/20.
+ */
+public class Angerofolddriver extends Skills {
+    public Angerofolddriver() {
+        super("Anger of Old Driver(-50)", 50);
+    }
+}
---
+++ src/Skills/CuiLaJiTuiBao.java
@@ -0,0 +1,10 @@
+package Skills;
+
+/**
+ * Created by HKLLC on 2016/9/20.
+ */
+public class CuiLaJiTuiBao extends Skills {
+    public CuiLaJiTuiBao() {
+        super("NO.3 CuiLaJiTuiBao(+60)", -60);
+    }
+}
