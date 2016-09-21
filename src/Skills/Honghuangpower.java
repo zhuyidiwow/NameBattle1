802f7fdc509066deb06c428a099867b0bfe01e3a
HELLOooWORLD <535140575@qq.com>
2016-09-21 22:57:56 +0800

    111
    111

---
+++ src/Skills/Honghuangpower.java
@@ -0,0 +1,10 @@
+package Skills;
+
+/**
+ * Created by HKLLC on 2016/9/20.
+ */
+public class Honghuangpower extends Skills {
+    public Honghuangpower() {
+        super("HongHuang Power(-80)", 80);
+    }
+}
